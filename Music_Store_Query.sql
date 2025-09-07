d728bb35c210e7d7ff9b42340c0d32a8775a4344
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-09-08 00:48:17 +0530

    Refactor README formatting and remove unnecessary comments from SQL files

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -21,6 +21,7 @@ ORDER BY c DESC
 SELECT total 
 FROM invoice
 ORDER BY total DESC
+LIMIT 3
 
 
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
@@ -44,9 +45,6 @@ GROUP BY customer.customer_id
 ORDER BY total_spending DESC
 LIMIT 1;
 
-
-
-
 /* Question Set 2 - Moderate */
 
 /* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
@@ -64,7 +62,6 @@ WHERE track_id IN(
 	WHERE genre.name LIKE 'Rock'
 )
 ORDER BY email;
-
 
 /* Method 2 */
 
@@ -102,9 +99,6 @@ WHERE miliseconds > (
 	FROM track )
 ORDER BY miliseconds DESC;
 
-
-
-
 /* Question Set 3 - Advance */
 
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
@@ -125,6 +119,7 @@ WITH best_selling_artist AS (
 	ORDER BY 3 DESC
 	LIMIT 1
 )
+
 SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
 FROM invoice i
 JOIN customer c ON c.customer_id = i.customer_id
@@ -134,7 +129,6 @@ JOIN album alb ON alb.album_id = t.album_id
 JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
 GROUP BY 1,2,3,4
 ORDER BY 5 DESC;
-
 
 /* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
 with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
@@ -157,8 +151,6 @@ WITH popular_genre AS
 	ORDER BY 2 ASC, 1 DESC
 )
 SELECT * FROM popular_genre WHERE RowNo <= 1
-
-
 /* Method 2: : Using Recursive */
 
 WITH RECURSIVE
@@ -224,7 +216,3 @@ ON cc.billing_country = ms.billing_country
 WHERE cc.total_spending = ms.max_spending
 ORDER BY 1;
 
-
-/* source: www.youtube.com/@RishabhMishraOfficial */
-
-/* Thank You :) */
